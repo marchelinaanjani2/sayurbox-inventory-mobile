ebce29e9cf1e44c11070f98223dda1794d2d908c
Marchelina Anjani S <marselinaanjanisaputri23@gmail.com>
2023-11-05 15:17:20 +0700

    Tugas 7 update

--- lib/menu.dart
+++ lib/menu.dart
@@ -4,9 +4,10 @@ class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
 
   final List<ShopItem> items = [
-    ShopItem("Lihat Item", Icons.checklist),
-    ShopItem("Tambah Item", Icons.add_shopping_cart),
-    ShopItem("Logout", Icons.logout),
+    ShopItem("Lihat Item", Icons.checklist, Color.fromARGB(255, 69, 129, 86)),
+    ShopItem("Tambah Item", Icons.add_shopping_cart,
+        Color.fromARGB(255, 28, 75, 29)),
+    ShopItem("Logout", Icons.logout, Color.fromARGB(255, 69, 208, 201)),
   ];
 
   @override
@@ -14,7 +15,7 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'SayurBox Inventory',
+          'PBP A',
         ),
       ),
       body: SingleChildScrollView(
@@ -28,7 +29,7 @@ class MyHomePage extends StatelessWidget {
                 padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
                 // Widget Text untuk menampilkan tulisan dengan alignment center dan style yang sesuai
                 child: Text(
-                  'PBP Shop', // Text yang menandakan toko
+                  'SayurBox Inventory', // Text yang menandakan toko
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 30,
@@ -61,30 +62,29 @@ class MyHomePage extends StatelessWidget {
 class ShopItem {
   final String name;
   final IconData icon;
+  final Color color; // Menambah properti warna
 
-  ShopItem(this.name, this.icon);
+  ShopItem(this.name, this.icon, this.color);
 }
 
 class ShopCard extends StatelessWidget {
   final ShopItem item;
 
-  const ShopCard(this.item, {super.key}); // Constructor
+  const ShopCard(this.item, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.indigo,
+      color: item.color, // Menggunakan warna dari objek ShopItem
       child: InkWell(
-        // Area responsive terhadap sentuhan
         onTap: () {
-          // Memunculkan SnackBar ketika diklik
           ScaffoldMessenger.of(context)
             ..hideCurrentSnackBar()
             ..showSnackBar(SnackBar(
-                content: Text("Kamu telah menekan tombol ${item.name}!")));
+              content: Text("Kamu telah menekan tombol ${item.name}!"),
+            ));
         },
         child: Container(
-          // Container untuk menyimpan Icon dan Text
           padding: const EdgeInsets.all(8),
           child: Center(
             child: Column(
